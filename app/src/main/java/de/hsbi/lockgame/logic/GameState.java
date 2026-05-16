9407615953d6c2bc69cc82b4d1f5cb0166a0d4f2
Maxim Pass <4Max0@proton.me>
2026-05-16 14:43:54 +0200

    GameState vervollstaendigen

--- app/src/main/java/de/hsbi/lockgame/logic/GameState.java
+++ app/src/main/java/de/hsbi/lockgame/logic/GameState.java
@@ -5,44 +5,62 @@ import de.hsbi.lockgame.model.*;
 import java.util.List;
 
 public final class GameState {
+    private Level level;
+    private Snake snake;
+    private List<Pin> pins;
+    private Status status;
+    private Direction pendingDirection;
 
     public GameState(
             Level level, Snake snake, List<Pin> pins, Status status, Direction pendingDirection) {
         // TODO: lege einen neuen GameState mit den übergebenen Informationen an
-        throw new UnsupportedOperationException("method not implemented yet");
+        this.level = level;
+        this.snake = snake;
+        this.pins = pins;
+        this.status = status;
+        this.pendingDirection = pendingDirection;
+        // throw new UnsupportedOperationException("method not implemented yet");
     }
 
     public Level level() {
         // TODO: Getter
-        throw new UnsupportedOperationException("method not implemented yet");
+        return this.level;
+        // throw new UnsupportedOperationException("method not implemented yet");
     }
 
     public Snake snake() {
         // TODO: Getter
-        throw new UnsupportedOperationException("method not implemented yet");
+        return this.snake;
+        // throw new UnsupportedOperationException("method not implemented yet");
     }
 
     public List<Pin> pins() {
         // TODO: Getter
-        throw new UnsupportedOperationException("method not implemented yet");
+        return this.pins;
+        // throw new UnsupportedOperationException("method not implemented yet");
     }
 
     public Status status() {
         // TODO: Getter
-        throw new UnsupportedOperationException("method not implemented yet");
+        return this.status;
+        // throw new UnsupportedOperationException("method not implemented yet");
     }
 
     public Direction pendingDirection() {
         // TODO: Getter
-        throw new UnsupportedOperationException("method not implemented yet");
+        return this.pendingDirection;
+        // throw new UnsupportedOperationException("method not implemented yet");
     }
 
     public GameState tick() {
         // TODO: diese Methode lässt das Spiel einen Schritt laufen (berechnet den Spielzustand im
         // nächsten Schritt)
 
-        // TODO: early exit: wenn das Spiel nicht läuft oder keine Blickrichtung gesetzt ist: keine
+        // TODO: early exit: wenn das Spiel nicht läuft oder keine Blickrichtung gesetzt ist: kein
         // Änderung
+        if (!this.status.isRunning() || this.pendingDirection == null) {
+            return this;
+        }
 
         // TODO: prüfe die folgenden Bedingungen:
         // (a) Schlange würde das Spielfeld verlassen: Spiel verloren
@@ -50,15 +68,55 @@ public final class GameState {
         // "none")
         // (c) Schlange beisst sich: Spiel verloren
         // (d) Schlange würde auf einen Pin gehen (Pin bereits gesetzt oder Schlange kommt nicht in
-        // der
-        // Aktivierungsrichtung): Blockiert (keine Bewegung, Blickrichtung "none")
-
+        // der Aktivierungsrichtung): Blockiert (keine Bewegung, Blickrichtung "none")
+        Position nextPosition = this.snake.nextHead(pendingDirection);
+        Pin nextPositionPin = null;
+        for (Pin pin : this.pins) {
+            if (pin.position().equals(nextPosition)) {
+                nextPositionPin = pin;
+                break;
+            }
+        }
+        if (!level.isInside(nextPosition)) {
+            this.status = Status.LOST_OUT_OF_BOUNDS;
+        } else if (level.cellAt(nextPosition) == CellType.WALL) {
+            this.pendingDirection = null;
+        } else if (snake.occupies(nextPosition)) {
+            this.status = Status.LOST_SELF_COLLISION;
+        } else if (nextPositionPin != null
+                && (nextPositionPin.state().isSet()
+                        || this.pendingDirection != nextPositionPin.activationDirection())) {
+            this.pendingDirection = null;
+        }
         // TODO: aktiviere einen noch nicht gesetzten Pin, wenn die Schlange in der richtigen
-        // Richtung
-        // auf den Pin gehen würde (die Schlange darf dabei aber nicht auf den Pin gehen)
+        // Richtung auf den Pin gehen würde (die Schlange darf dabei aber nicht auf den Pin gehen)
+        boolean canActivatePin =
+                nextPositionPin != null
+                        && !nextPositionPin.state().isSet()
+                        && this.pendingDirection == nextPositionPin.activationDirection();
+        if (canActivatePin) {
+            this.pins.remove(nextPositionPin);
+            nextPositionPin = nextPositionPin.withState(Pin.State.HIGH);
+            this.pins.add(nextPositionPin);
+        }
+
+        if (this.status.isRunning()) {
+            // Check if all pins are set else keep running the game
+            for (Pin pin : this.pins) {
+                this.status = Status.WON;
+                if (!pin.state().isSet()) {
+                    this.status = Status.RUNNING;
+                    break;
+                }
+            }
+        }
 
         // TODO: anderenfalls: bewege die Schlange um einen Schritt in Blickrichtung (falls gesetzt)
-        throw new UnsupportedOperationException("method not implemented yet");
+        if (this.pendingDirection != null) {
+            this.snake.grow(pendingDirection);
+        }
+        return this;
+        // throw new UnsupportedOperationException("method not implemented yet");
     }
 
     public enum Status {
